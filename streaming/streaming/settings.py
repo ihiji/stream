00ffe3a831c37b22dd686fff5b2934fb2e0b426b
Byron Lantzsch <modxero@gmail.com>
2012-08-02 13:30:05 -0500

    added config to settings.py for jenkins business

--- streaming/streaming/settings.py
+++ streaming/streaming/settings.py
@@ -200,3 +200,22 @@ SESSION_ENGINE = 'mongoengine.django.sessions'
 
 from mongoengine import connect
 connect('stream')
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.with_coverage',
+    #'django_jenkins.tasks.django_tests',   # select one django or
+    'django_jenkins.tasks.dir_tests',      # directory tests discovery
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_jslint',
+    'django_jenkins.tasks.run_csslint',
+    #'django_jenkins.tasks.run_sloccount',
+    #'django_jenkins.tasks.lettuce_tests',
+)
+
+JENKINS_TEST_RUNNER = "django_jenkins.runner.CITestSuiteRunner"
+
+PROJECT_APPS = (
+    'smartview_base',
+)
