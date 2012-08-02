501eaf76e2e6ea2c77e11b00df49cb60ec510a7e
Stuart Rench <stuartr@ihiji.com>
2012-08-02 16:22:19 -0500

    Added a User reference field and a unicode function to print out the Stream name

--- stream_app/models.py
+++ stream_app/models.py
@@ -1,4 +1,5 @@
 from mongoengine import *
+from mongoengine.django.auth import User
 
 class Stream(Document):
     name    = StringField(min_length=1, max_length=80, help_text="A Name to find your stream")
@@ -6,3 +7,7 @@ class Stream(Document):
     start_time = DateTimeField()
     end_time   = DateTimeField()
     entry_count = IntField(min_value=0)
+    user = ReferenceField('User', reverse_delete_rule=CASCADE)
+
+    def __unicode__(self):
+        return self.name
