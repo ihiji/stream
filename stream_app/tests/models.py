112abcb1169cc190db745f08ebdace8ff51574da
Stuart Rench <stuartr@ihiji.com>
2012-08-03 08:45:56 -0500

    clean up bad syntax.  trying to get a clean build

--- stream_app/tests/models.py
+++ stream_app/tests/models.py
@@ -3,7 +3,5 @@ import mongoengine
 
 class StreamAppTestCase(TestCase):
     def setUp(self):
-        super(StreamAppTestCase, self).setUp())
+        super(StreamAppTestCase, self).setUp()
         self.stream1 = Stream.objects.get(pk='501acc4b70a2e42ec1e05092')
-
-    def test_name_lenght(self):
