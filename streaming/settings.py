563f87850148d27aff45d87deee974e943edda9b
Byron Lantzsch <modxero@gmail.com>
2012-08-06 16:33:02 -0500

    check third_party directory for external static files

--- streaming/settings.py
+++ streaming/settings.py
@@ -102,6 +102,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(SITE_ROOT, '..', 'third_party'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
